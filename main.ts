9068d60a553494b14b0e087c352a92784eec9fb5
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-26 09:49:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    Player.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    Player.change(LedSpriteProperty.X, 1)
+})
+let Player: game.LedSprite = null
+Player = game.createSprite(2, 2)
+let Enemy = game.createSprite(randint(0, 4), 2)
+basic.forever(function () {
+	
+})
